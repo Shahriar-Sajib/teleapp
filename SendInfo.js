619b51a42542465d97f49ac63eed50590f088c9f
Shahriar-Sajib <146087362+Shahriar-Sajib@users.noreply.github.com>
2024-09-07 23:10:35 +0600

    Add files via upload

--- SendInfo.js
+++ SendInfo.js
@@ -1,23 +1,22 @@
 const botToken = '7229842427:AAHpGOEpOmr3G_DNL04JpxROEohyFwa-dXI';
-const myText ="User Id and Refer Code :"+localStorage.getItem("userId")+"More Info will be here soon!";
+const myText = "User Id and Refer Code :" + localStorage.getItem("userId") + "\n More Info will be here soon!";
 function showInfo() {
   tg.showAlert(myText)
-  // if (localStorage.getItem("userId") != null) {
-//     tg.showAlert("Check the bot")
-//     fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         chat_id: localStorage.getItem("userId"),
-//         text: myText
-//       })
-//     })
-//     //.c                  atch(error => tg.showAlert('Error sending message:', error));
-//   } else{
-//     tg.close()
-//     tg.showAlert("something wrong")
-//   }
-// 
+  if (localStorage.getItem("userId") != null) {
+    tg.showAlert("Check the bot")
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: localStorage.getItem("userId"),
+        text: myText
+      })
+    })
+ //   //.catch(error => tg.showAlert('Error sending message:', error));
+  } else {
+    tg.close()
+    tg.showAlert("something wrong")
+  }
 }
